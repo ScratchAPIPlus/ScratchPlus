a414cabd0e279f264802b2a20332a943c7ea9494
Laiten Kelly <laitenkelly@outlook.com>
2022-04-02 16:13:04 -0400

    Work on users page and navbar favicon.ico

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, send_from_directory
+import requests, os
 app = Flask(__name__)
 
 MAIN_URL = "https://scratchapiplus.mel0n7.repl.co/"
@@ -7,6 +7,10 @@ MAIN_URL = "https://scratchapiplus.mel0n7.repl.co/"
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/info')
 def info():
